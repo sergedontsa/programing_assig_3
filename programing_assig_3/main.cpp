f8150024e1ecd5eff427ba5fd654ec8400494ad3
sergedontsa <63218303+sergedontsa@users.noreply.github.com>
2021-11-05 17:56:31 -0400

    Initial Commit

---
+++ programing_assig_3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  programing_assig_3
+//
+//  Created by Serge Dontsa on 2021-11-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
